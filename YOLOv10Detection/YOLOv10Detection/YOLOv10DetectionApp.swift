26b6f3d5cc2e72e8bb8e258b744b4d4dfc3e4d9b
Divya <113543653+DivyaTalla01@users.noreply.github.com>
2024-10-26 14:19:03 -0500

    Add files via upload

---
+++ YOLOv10Detection/YOLOv10Detection/YOLOv10DetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  YOLOv10DetectionApp.swift
+//  YOLOv10Detection
+//
+//  Created by Divya Talla on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct YOLOv10DetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
